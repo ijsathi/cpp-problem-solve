6d4b2bbb384c87dff44bbd4b952b80f20d8c8a9f
unknown <ijsathi2001rs@gmail.com>
2023-11-15 10:05:49 +0600

    return array from function

---
+++ return_arry_fuction.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int *fun()
+{
+    int *arr = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    return arr;
+}
+int main()
+{
+    int *a = fun();
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
